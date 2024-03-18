2ff5f0278303a6426dce33ad0fccf59e10274b8f
Mooka <47282413+mooka007@users.noreply.github.com>
2024-03-18 16:43:39 +0000

    update

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../Home/index";
-import LOL from "../Home/lol";
-// import "react-tooltip/dist/react-tooltip.css"; 
+import Home from "../Home/index" ;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="**" element={<LOL/>} />
-        <Route path="*" element={<LOL/>} />
       </Routes>
     </BrowserRouter>
   );
